5fd405c3cdbb3969f37ab11c5118a88a9e870f65
mihazed@gmail.com <michaelzi@matrix.co.il>
2020-01-01 15:59:34 +0200

    Updates

---
+++ precache-manifest.e0812f54b710091260018a6b3d062da4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f59e2e3b0bf709c8df55d2cb7c17ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e88a3dcc92a7e362d2c",
+    "url": "/monsters-rolodex/static/css/main.ce49955b.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e88a3dcc92a7e362d2c",
+    "url": "/monsters-rolodex/static/js/main.2a206924.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
